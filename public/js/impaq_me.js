e30f6b0d4f82e18979c779564330994c31b75d76
Jason Karns <jason@karns.name>
2013-04-19 11:52:11 -0400

    namespace like a boss

--- public/js/impaq_me.js
+++ public/js/impaq_me.js
@@ -2,7 +2,8 @@
   window.impaq_me = $.extend({}, window.impaq_me, {
 
     init: function(){
-            impaq_me.button = $("<button>ImpaqMe</button>").appendTo('body').click(impaq_me.step1);
+            this.button = $("<button>ImpaqMe</button>").appendTo('body');
+            this.button.click($.proxy(this.step1, this));
           },
 
     preload_sdks: function(){
@@ -13,19 +14,19 @@
 
                     // load twitter and facebook SDKs
                     //FIXME ensure SDK only loaded once: if(!$("#twitter-wjs").size())
-                    impaq_me.twitter = $.getScript("//platform.twitter.com/widgets.js");
+                    this.twitter = $.getScript("//platform.twitter.com/widgets.js");
                     //FIXME ensure SDK only loaded once: if(!$("#facebook-jssdk").size())
-                    impaq_me.facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
+                    this.facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
                   },
 
     step1: function(){
             $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
             $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
 
-            impaq_me.twitter.done(function(){
+            this.twitter.done(function(){
               twttr.widgets.load();
             });
-            impaq_me.facebook.done(function(){
+            this.facebook.done(function(){
               FB.XFBML.parse();
             });
           }
