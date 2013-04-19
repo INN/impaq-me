d2536032b58d214510fd63bae13ddb8e11b71ccf
Jason Karns <jason@karns.name>
2013-04-19 10:12:26 -0400

    Load SDKs via jQuery.getScript and use promises to safely execute the parsing

--- public/js/impaq_me.js
+++ public/js/impaq_me.js
@@ -1,14 +1,26 @@
 (function($){
 
-  // to parse/insert new twitter/fb buttons after the SDKs have loaded, call twttr.widgets.load(); and FB.XFBML.parse();
-
-  // insert twitter tweet button
-  $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
-  // insert facebook 'like' button
-  $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
+  //FIXME: shouldn't alter client's jquery env
+  $.ajaxSetup({
+      cache: true
+  });
 
   // load twitter and facebook SDKs
-  $('body').append('<script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"));</script>');
-  $('body').append('<script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/en_US/all.js#xfbml=1";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","facebook-jssdk"));</script>');
+  // if(!$("#twitter-wjs").size())
+  var twitter = $.getScript("//platform.twitter.com/widgets.js")
+  // if(!$("#facebook-jssdk").size())
+  var facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
+
+
+  // insert buttons
+  $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
+  $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
+
+  twitter.done(function(){
+    twttr.widgets.load();
+  });
+  facebook.done(function(){
+    FB.XFBML.parse();
+  });
 
 })(jQuery);
