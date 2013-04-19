1dbe1fbe1ea869f8e43a44efc224df1074c01203
Jason Karns <jason@karns.name>
2013-04-19 10:46:12 -0400

    re-org and move share stuff into 'step1'

--- public/js/impaq_me.js
+++ public/js/impaq_me.js
@@ -1,26 +1,39 @@
 (function($){
+  window.impaq_me = $.extend({}, window.impaq_me, {
 
-  //FIXME: shouldn't alter client's jquery env
-  $.ajaxSetup({
-      cache: true
+    init: function(){
+            impaq_me.button = $("<button>ImpaqMe</button>").appendTo('body').click(impaq_me.step1);
+          },
+
+    preload_sdks: function(){
+                    //FIXME: shouldn't alter client's jquery env
+                    $.ajaxSetup({
+                      cache: true
+                    });
+
+                    // load twitter and facebook SDKs
+                    //FIXME ensure SDK only loaded once: if(!$("#twitter-wjs").size())
+                    impaq_me.twitter = $.getScript("//platform.twitter.com/widgets.js");
+                    //FIXME ensure SDK only loaded once: if(!$("#facebook-jssdk").size())
+                    impaq_me.facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
+                  },
+
+    step1: function(){
+            $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
+            $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
+
+            impaq_me.twitter.done(function(){
+              twttr.widgets.load();
+            });
+            impaq_me.facebook.done(function(){
+              FB.XFBML.parse();
+            });
+          }
   });
 
-  // load twitter and facebook SDKs
-  // if(!$("#twitter-wjs").size())
-  var twitter = $.getScript("//platform.twitter.com/widgets.js")
-  // if(!$("#facebook-jssdk").size())
-  var facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
 
+  impaq_me.init();
+  impaq_me.preload_sdks();
 
-  // insert buttons
-  $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
-  $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
-
-  twitter.done(function(){
-    twttr.widgets.load();
-  });
-  facebook.done(function(){
-    FB.XFBML.parse();
-  });
 
 })(jQuery);
