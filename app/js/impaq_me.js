d23c305adf8336020557ac106a27af50f7f4083a
Jason Karns <jason@karns.name>
2013-04-22 14:34:39 -0400

    Use vanillaJS and an IFrame

--- app/js/impaq_me.js
+++ app/js/impaq_me.js
@@ -1,51 +1,30 @@
-//FIXME: currently depends on jQuery being loaded in host page
-(function($){
-  window.impaq_me = $.extend({}, window.impaq_me, {
+(function(){
+  window.impaq_me = {
 
     init: function(){
-            this.button = $("<button>ImpaqMe</button>").appendTo('body');
-            this.button.click($.proxy(this.step1, this));
-          },
+      this.button = document.createElement("button");
+      this.button.textContent = "ImpaqMe";
 
-    preload_sdks: function(){
-                    //FIXME: shouldn't alter client's jquery env
-                    $.ajaxSetup({
-                      cache: true
-                    });
+      this.script().parentNode.insertBefore(this.button, this.script());
 
-                    // load twitter and facebook SDKs
-                    //FIXME ensure SDK only loaded once: if(!$("#twitter-wjs").size())
-                    this.twitter = $.getScript("//platform.twitter.com/widgets.js");
-                    //FIXME ensure SDK only loaded once: if(!$("#facebook-jssdk").size())
-                    this.facebook = $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1");
-                  },
+      // this.button.click($.proxy(this.step1, this));
+    },
 
-    step1: function(){
-            $('body').append('<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
-            $('body').append('<div class="fb-like" data-width="90" data-layout="button_count"></div>');
+    script: function(){
+      var scripts = document.getElementsByTagName('script');
+      return scripts[scripts.length-1];
+    },
 
-            this.twitter.done(function(){
-              twttr.widgets.load();
-            });
-            this.facebook.done(function(){
-              FB.XFBML.parse();
-            });
-          }
-  });
+
+    createIframe: function(){
+      this.iframe = document.createElement("iframe");
+      this.iframe.src = this.script().src + "/../../index.html";
+      this.script().parentNode.insertBefore(this.iframe, this.script());
+    }
+  };
 
 
   impaq_me.init();
-  impaq_me.preload_sdks();
+  impaq_me.createIframe();
 
-
-})(jQuery);
-
-
-// ImpaqMe.registerShareService({
-//   name: added to .services
-//   sdk: 
-//     string of script to load with getScript, (promise() returned as .services.<name>.sdk)
-//     or
-//     function to be executed, context set as ImpaqMe
-//   step1
-//     function to be executed when the 
+})();
