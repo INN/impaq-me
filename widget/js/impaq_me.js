19e76584d5fe58f7e75ac153be8f8f2ad34d2c15
Jason Karns <jason@karns.name>
2013-04-23 12:28:23 -0400

    Update things with stuff

--- widget/js/impaq_me.js
+++ widget/js/impaq_me.js
@@ -4,6 +4,7 @@
     init: function(){
       this.button = document.createElement("button");
       this.button.textContent = "ImpaqMe";
+      this.button.style.display = "block";
 
       this.script().parentNode.insertBefore(this.button, this.script());
 
@@ -19,6 +20,11 @@
     createIframe: function(){
       this.iframe = document.createElement("iframe");
       this.iframe.src = this.script().src + "/../../index.html";
+      this.iframe.style.border = "0";
+      this.iframe.style.outline = "1px solid red";
+      this.iframe.style.width = "100%";
+      this.iframe.style.height = "300px";
+      this.iframe.style.marginTop = ".5em";
       this.script().parentNode.insertBefore(this.iframe, this.script());
     }
   };
