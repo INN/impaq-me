67fbbc4a35011807f6aa561dc767cc6efca40846
Jason Karns <jason@karns.name>
2013-04-24 11:45:31 -0400

    restructure widget with jquery

--- app/js/widget.js
+++ app/js/widget.js
@@ -4,36 +4,35 @@
     me: {
 
       init: function(){
-        this.button = document.createElement("button");
-        this.button.textContent = "ImpaqMe";
-        this.button.style.display = "block";
+        var button = $('<button style="background:red; color: white; width:100%">ImpaqMe</button>')
+          .on('click', $.proxy(this.createIframe, this));
 
-        this.script().parentNode.insertBefore(this.button, this.script());
-
-        // this.button.click($.proxy(this.step1, this));
+        $('<div class="impaq-me-widget"></div>')
+          .append(button)
+          .replaceAll(".impaq-me-button");
       },
 
-      script: function(){
-        var scripts = document.getElementsByTagName('script');
-        return scripts[scripts.length-1];
-      },
+      createIframe: function(event){
+        var iframe = $('<iframe src="http://localhost:8003/index.html"></iframe>')
+          .css({
+            position: 'absolute',
+            top:0,
+            left:0,
+            border:0,
+            outline: '1px solid red',
+            width: '100%',
+            height: '200px'
+          });
 
-
-      createIframe: function(){
-        this.iframe = document.createElement("iframe");
-        this.iframe.src = "http://localhost:8003/index.html";
-        this.iframe.style.border = "0";
-        this.iframe.style.outline = "1px solid red";
-        this.iframe.style.width = "100%";
-        this.iframe.style.height = "300px";
-        this.iframe.style.marginTop = ".5em";
-        this.script().parentNode.insertBefore(this.iframe, this.script());
+        $('<div style="position:relative">')
+          .append(iframe)
+          .insertAfter(event.target);
       }
     }
   };
 
 
   Impaq.me.init();
-  Impaq.me.createIframe();
+  // Impaq.me.createIframe();
 
 })(jQuery.noConflict(true));
