afaf05636cd0b8bdf9b2a01c41eba61d0b3741bf
Jason Karns <jason@karns.name>
2013-04-24 10:46:30 -0400

    Namespace jQuery into Impaq

--- app/js/widget.js
+++ app/js/widget.js
@@ -1,36 +1,39 @@
-(function(){
-  window.impaq_me = {
+(function($){
+  window.Impaq = {
+    $: $,
+    me: {
 
-    init: function(){
-      this.button = document.createElement("button");
-      this.button.textContent = "ImpaqMe";
-      this.button.style.display = "block";
+      init: function(){
+        this.button = document.createElement("button");
+        this.button.textContent = "ImpaqMe";
+        this.button.style.display = "block";
 
-      this.script().parentNode.insertBefore(this.button, this.script());
+        this.script().parentNode.insertBefore(this.button, this.script());
 
-      // this.button.click($.proxy(this.step1, this));
-    },
+        // this.button.click($.proxy(this.step1, this));
+      },
 
-    script: function(){
-      var scripts = document.getElementsByTagName('script');
-      return scripts[scripts.length-1];
-    },
+      script: function(){
+        var scripts = document.getElementsByTagName('script');
+        return scripts[scripts.length-1];
+      },
 
 
-    createIframe: function(){
-      this.iframe = document.createElement("iframe");
-      this.iframe.src = "http://localhost:8003/index.html";
-      this.iframe.style.border = "0";
-      this.iframe.style.outline = "1px solid red";
-      this.iframe.style.width = "100%";
-      this.iframe.style.height = "300px";
-      this.iframe.style.marginTop = ".5em";
-      this.script().parentNode.insertBefore(this.iframe, this.script());
+      createIframe: function(){
+        this.iframe = document.createElement("iframe");
+        this.iframe.src = "http://localhost:8003/index.html";
+        this.iframe.style.border = "0";
+        this.iframe.style.outline = "1px solid red";
+        this.iframe.style.width = "100%";
+        this.iframe.style.height = "300px";
+        this.iframe.style.marginTop = ".5em";
+        this.script().parentNode.insertBefore(this.iframe, this.script());
+      }
     }
   };
 
 
-  impaq_me.init();
-  impaq_me.createIframe();
+  Impaq.me.init();
+  Impaq.me.createIframe();
 
-})();
+})(jQuery.noConflict(true));
