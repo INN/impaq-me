ddd40bf1c5ce0c53caabd2bff694c37504f00d92
Jason Karns <jason@karns.name>
2013-04-25 12:00:23 -0400

    Add jQuery. Add script to get Tweet callback and post to ImpaqMeAPI

---
+++ app/js/app.js
@@ -0,0 +1,17 @@
+window.twttr = (t = { _e: [], ready: function(f){ t._e.push(f); } });
+
+twttr.ready(function(){
+  twttr.events.bind('tweet', function(event){
+    $.post(
+      "http://impaq-backend.herokuapp.com/shares",
+      {
+        campaign_id: 1,
+        share_method: 'twitter',
+        message: "hardcoded message"
+      },
+      function(data, textStatus, jqXHR){
+        debugger;
+      }
+    );
+  });
+});
