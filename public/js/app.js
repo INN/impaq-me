b9e1010b1dd8be61b13cf1978b450940c418f24a
Zach <briggszj@gmail.com>
2013-04-25 16:00:12 -0400

    non-minified app.js

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+window.twttr = (t = { _e: [], ready: function(f){ t._e.push(f); } });
+
+twttr.ready(function(){
+    console.log("twitter loaded");
+  twttr.events.bind('tweet', function(event){
+    console.log("tweeted");
+    $.post(
+      "/shares",
+      {
+        campaign_id: 1,
+        share_method: 'twitter',
+        message: "hardcoded message"
+      },
+      function(data, textStatus, jqXHR){
+        console.log(textStatus);
+        console.log(data);
+        console.log(jqXHR);
+      }
+    );
+  });
+});
