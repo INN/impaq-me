0e57b82696d352011c24546ffe558c2fb22a40f5
Jason Karns <jason@karns.name>
2013-04-29 14:53:23 -0400

    Value per share should be an integer

--- app/services/iframe.rb
+++ app/services/iframe.rb
@@ -7,7 +7,7 @@ class Iframe
       foundation_name: campaign.foundation_name,
       publisher_name: campaign.publisher_name,
       testimonial: testimonial,
-      value_per_share: campaign.value_per_share,
+      value_per_share: campaign.value_per_share.to_i
     }.merge campaign_meter
   end
 end
