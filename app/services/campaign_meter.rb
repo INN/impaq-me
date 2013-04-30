0eb22c6793e9bbd05c7c8bacee4f5af6830cdb5e
Jason Karns <jason@karns.name>
2013-04-29 21:45:55 -0400

    Move logic out of template and into view object

--- app/services/campaign_meter.rb
+++ app/services/campaign_meter.rb
@@ -2,6 +2,12 @@ class CampaignMeter
   def self.for_campaign id
     goal = Campaign.goal_for id: id
     total = Share.total_for campaign_id: id
-    { campaign_id: id, goal: goal, total: total }
+    percent = total/goal * 100
+    {
+      campaign_id: id,
+      goal: goal,
+      total: total,
+      percent: percent
+    }
   end
 end
