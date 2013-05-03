20d662742e89fb04025d31bf335b439d625ef9b4
Jason Karns <jason@karns.name>
2013-05-03 00:41:32 -0400

    Major changes to widget. Use templates and web fonts

--- app/js/widget.js
+++ app/js/widget.js
@@ -1,35 +1,33 @@
 (function($, _){
-  window.Impaq = {
-    $: $,
-    _: _,
+  $.extend(window.Impaq, {$:$, _:_});
+
+  $.extend(window.Impaq, {
     me: {
 
-      init: function(){
-        var button = $('<button style="background:#ED1C24; color: white; width:100%; border:0; cursor:pointer;">ImpaqMe</button>')
-          .on('click', $.proxy(this.createIframe, this));
-
-        $('<div class="impaq-me-widget"></div>')
-          .append(button)
-          .replaceAll(".impaq-me-button");
+      config: {
+        button_text: 'Donate by Sharing',
+        iframe_src: '//localhost:3000'
       },
 
-      createIframe: function(event){
-        var iframe = $('<iframe src="//localhost:3000?uri='+ encodeURIComponent(location) +'"></iframe>')
-          .css({
-            position: 'absolute',
-            top:0,
-            left:0,
-            border:0,
-            width: '100%',
-            height: '190px'
-          });
+      init: function(){
+        var marker = $(".impaq-me-button");
 
-        $('<div style="position:relative">')
-          .append(iframe)
-          .insertAfter(event.target);
+        var data = $.extend({}, this.config, {
+          article_url: this.articleURL(marker)
+        });
+
+        $(this.compileWidget(data)).replaceAll(marker);
+      },
+
+      compileWidget: function(data){
+        return Impaq.JST['app/templates/widget.us'](data);
+      },
+
+      articleURL: function(marker){
+        return marker.data('url') || $('link[rel=canonical]').attr('href') || location;
       }
     }
-  };
+  });
 
 
   Impaq.me.init();
