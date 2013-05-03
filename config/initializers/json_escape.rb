896af1230fd51f9bf2ce6f8b9e48387aeb1a0ca4
Jason Karns <jason@karns.name>
2013-05-03 14:27:04 -0400

    Pull out Backbone templates. Patch json_escape

---
+++ config/initializers/json_escape.rb
@@ -0,0 +1,9 @@
+class ActionView::Base
+  def json_escape(s)
+    puts "ESCAPING"
+    result = s.to_s.gsub('/', '\/')
+    s.html_safe? ? result.html_safe : result
+  end
+
+  alias j json_escape
+end
