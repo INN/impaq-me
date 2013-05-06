5eb0736936c5186f88ca811c1196edbee4f92b24
Jason Karns <jason@karns.name>
2013-05-06 11:36:13 -0400

    Wire up the router navigation to the 'share' event

--- app/assets/javascripts/views/share.js
+++ app/assets/javascripts/views/share.js
@@ -6,4 +6,21 @@ window.app.views.Share = Backbone.View.extend({
     this.$el.html(this.template(impaqme));
     return this;
   }
+
+});
+
+twttr.ready(function(){
+  twttr.events.bind('tweet', function(event){
+    console.info("tweet event", event);
+    app.services.Share.record_tweet(impaqme.campaign_id);
+    app.router.navigate('shared', {trigger:true});
+  });
+});
+
+fb.ready(function(){
+  FB.Event.subscribe('edge.create', function(response) {
+    console.info("fb like event", response);
+    app.services.Share.record_like(impaqme.campaign_id);
+    app.router.navigate('shared', {trigger:true});
+  });
 });
