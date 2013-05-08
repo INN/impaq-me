6f42beb80c7ce0c9a4b015dc4fde8f62ceff2902
Jason Karns <jason@karns.name>
2013-05-07 22:23:39 -0400

    Bind/Trigger FB/Twitter events through the Share view

--- app/assets/javascripts/services/share.js
+++ app/assets/javascripts/services/share.js
@@ -1,19 +1,14 @@
-window.app.services.Share = (function() {
-  var record_share = function(channel, campaign_id) {
+window.app.services.Share = {
+  record: function(channel, campaign_id) {
     return $.post("/shares", {
         share: {
           campaign_id: campaign_id,
-          share_method: channel
+          channel: channel
         }
       },
       function(data, textStatus, jqXHR){
         console.info(channel + " share recorded", textStatus, data, jqXHR);
       }
     );
-  };
-
-  return {
-    record_tweet: _.partial(record_share, 'twitter'),
-    record_like:  _.partial(record_share, 'facebook')
-  };
-})();
+  }
+};
--- app/assets/javascripts/views/share.js
+++ app/assets/javascripts/views/share.js
@@ -2,6 +2,16 @@ window.app.views.Share = Backbone.View.extend({
   id: 'share',
   template: JST['share'],
 
+  initialize: function(options){
+    this.on('like', _.partial(this.share, 'facebook', impaqme.campaign_id));
+    this.on('tweet', _.partial(this.share, 'twitter', impaqme.campaign_id));
+  },
+
+  share: function(channel, campaign_id){
+    app.services.Share.record(channel, campaign_id);
+    app.router.navigate('shared', {trigger:true});
+  },
+
   render: function(){
     this.$el.html(this.template(impaqme));
     return this;
@@ -11,16 +21,12 @@ window.app.views.Share = Backbone.View.extend({
 
 twttr.ready(function(){
   twttr.events.bind('tweet', function(event){
-    console.info("tweet event", event);
-    app.services.Share.record_tweet(impaqme.campaign_id);
-    app.router.navigate('shared', {trigger:true});
+    app.views.share.trigger('tweet');
   });
 });
 
 fb.ready(function(){
   FB.Event.subscribe('edge.create', function(response) {
-    console.info("fb like event", response);
-    app.services.Share.record_like(impaqme.campaign_id);
-    app.router.navigate('shared', {trigger:true});
+    app.views.share.trigger('like');
   });
 });
