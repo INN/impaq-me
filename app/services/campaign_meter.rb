69ebaffa69a917586e81500402bed9edf77e991b
Zach <briggszj@gmail.com>
2013-05-14 15:21:37 -0400

    hard limit on max

--- app/services/campaign_meter.rb
+++ app/services/campaign_meter.rb
@@ -1,7 +1,7 @@
 class CampaignMeter
   def self.for campaign
     shares = Share.total_for_campaign campaign.id
-    total = shares * campaign.value_per_share
+    total = max((shares * campaign.value_per_share), campaign.goal)
     percent = (total/campaign.goal * 100).round 2
     {
       campaign_id: campaign.id,
@@ -9,5 +9,13 @@ class CampaignMeter
       total: total.to_i,
       percent: percent
     }
+  end
+
+  def self.max generated, goal
+    if generated <= goal
+      generated
+    else
+      goal
+    end
   end
 end
