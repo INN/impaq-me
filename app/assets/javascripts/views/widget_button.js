1ea6183d4c84aaa14ddc47dfcd667f89dd23c67f
Jason Karns <jason@karns.name>
2013-05-20 22:48:32 -0400

    Change the widget button image in a more sane location

--- app/assets/javascripts/views/widget_button.js
+++ app/assets/javascripts/views/widget_button.js
@@ -3,10 +3,20 @@ window.app.views.WidgetButton = Backbone.View.extend({
     click: 'click'
   },
 
-  initialize: function(options){ },
+  initialize: function(options){
+    _.bindAll(this);
+
+    this.listenTo(this.model, {
+      'change:open':    this.openChanged
+    });
+  },
 
   click: function(e){
     this.model.set('open', !this.model.get('open'));
+  },
+
+  openChanged: function(model, open, options){
+    this.$el.toggleClass('opened', open);
   },
 
   render: function(){
