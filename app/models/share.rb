99ed6f481f74a894f3de73a8424bf3f50371c0bd
Zach <briggszj@gmail.com>
2013-05-21 23:26:07 -0400

    calulating total share value from cooldown based stored value

--- app/models/share.rb
+++ app/models/share.rb
@@ -5,7 +5,7 @@ class Share
   field :channel
   field :ip
   field :referer
-  field :value
+  field :value, default: 0
   field :timestamp, default: DateTime.now
 
   validates :campaign_id, presence: true
@@ -15,7 +15,7 @@ class Share
   belongs_to :campaign
 
   def self.total_for_campaign(campaign_id)
-    where(campaign_id: campaign_id).distinct(:ip).count
+    where(campaign_id: campaign_id).map(&:value).sum
   end
 
   def self.to_csv
