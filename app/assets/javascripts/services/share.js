d01733236666718b54e739d7330a59c4546173ec
Jason Karns <jason@karns.name>
2013-05-22 17:17:17 -0400

    Partially-apply the 'record' share events. Add social tracking to the analytics service

--- app/assets/javascripts/services/share.js
+++ app/assets/javascripts/services/share.js
@@ -1,11 +1,17 @@
 window.app.services.Share = {
-  record: function(channel) {
+  record: function(channel, campaign_id, article_url) {
     return $.post("/shares", {
       share: {
-        campaign_id: app.models.widget.get('campaign_id'),
-        article_url: app.models.widget.get('article_url'),
+        campaign_id: campaign_id,
+        article_url: article_url,
         channel: channel
       }
     });
   }
 };
+
+_(app.services.Share).extend({
+  recordTweet: _.partial(app.services.Share.record, 'twitter'),
+  recordLike:  _.partial(app.services.Share.record, 'facebook'),
+  recordEmail: _.partial(app.services.Share.record, 'email')
+});
