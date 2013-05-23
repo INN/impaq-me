a1796d1be0fa0746b435013b74cf4f35a5dc9128
Jason Karns <jason@karns.name>
2013-05-23 14:58:58 -0400

    shares of shares should not be 'new' urls

--- app/services/shortlink.rb
+++ app/services/shortlink.rb
@@ -3,6 +3,8 @@ require 'addressable/uri'
 class Shortlink
   def self.for_campaign_and_url(campaign, long_url)
 
+    long_url = remove_banner_toggle(long_url).to_s
+
     twitter = Link.find_or_create_by({
       campaign: campaign,
       long_url: long_url,
@@ -29,16 +31,29 @@ class Shortlink
   end
 
   def self.follow(link: link, remote_ip: remote_ip, referer: referer, user_agent: user_agent)
-    Addressable::URI.parse(link.long_url).tap do |url|
-      ClickThrough.create(
-        campaign: link.campaign,
-        article_url: url.to_s,
-        channel: link.channel,
-        ip: remote_ip,
-        referer: referer,
-        user_agent: user_agent
-      )
+    ClickThrough.create(
+      campaign: link.campaign,
+      article_url: link.long_url,
+      channel: link.channel,
+      ip: remote_ip,
+      referer: referer,
+      user_agent: user_agent
+    )
+    add_banner_toggle link.long_url
+  end
+
+  private
+
+  def self.remove_banner_toggle long_url
+    Addressable::URI.parse(long_url).tap do |url|
+      url.query_values = (url.query_values || {}).except('shared_via_impaq_me')
+    end
+  end
+
+  def self.add_banner_toggle long_url
+    Addressable::URI.parse(long_url).tap do |url|
       url.query_values = (url.query_values || {}).merge 'shared_via_impaq_me' => true
     end
   end
+
 end
