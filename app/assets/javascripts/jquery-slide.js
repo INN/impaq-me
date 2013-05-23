cca6c81e26e67ec00910d9b297a9f23deecac8a9
Jason Karns <jason@karns.name>
2013-05-23 11:35:42 -0400

    jQuery slide() plugin no longer needed

--- app/assets/javascripts/jquery-slide.js
+++
@@ -1,5 +0,0 @@
-$.fn.slide = function(){
-  var args = Array.prototype.slice.apply(arguments);
-  var direction = args.pop();
-  return direction ? $(this).slideDown.apply(this, args) : $(this).slideUp.apply(this, args);
-};
