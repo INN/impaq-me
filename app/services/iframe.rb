313a112c2f27d8970b5de0a109fcdad6bac1dd60
Matthew Boston <matthew.boston@gmail.com>
2013-11-22 17:36:30 -0500

    a more consistant method name for CampaignMeter

--- app/services/iframe.rb
+++ app/services/iframe.rb
@@ -4,7 +4,7 @@ class Iframe
   def self.bootstrap article, user_address
     article_url = Addressable::URI.heuristic_parse(article.url).normalize
     campaign = Campaign.find_by domains: article_url.host
-    campaign_meter = CampaignMeter.for campaign
+    campaign_meter = CampaignMeter.for_campaign campaign
     shortlink = Shortlink.for_campaign_and_url campaign, article_url.to_s
     sample_variant = SampleVariant.choose_from campaign.variants
 
