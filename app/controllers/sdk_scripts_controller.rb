ba82d57b83ebcb279c58272c9746d1ce7d82ccba
Justin Searls <searls@gmail.com>
2014-06-05 00:07:31 -0400

    Work around rails #94

--- app/controllers/sdk_scripts_controller.rb
+++ app/controllers/sdk_scripts_controller.rb
@@ -1,5 +1,12 @@
 class SdkScriptsController < ApplicationController
   def show
-    redirect_to self.class.helpers.asset_path("lineman/sdk/impaq-me-sdk.min.js")
+    sdk_path = self.class.helpers.asset_path("lineman/sdk/impaq-me-sdk.min.js")
+    if sdk_path =~ /^http/
+      # If this is true then we're on Rails 4.1+ and you can kill the else clause and this branch
+      # see https://github.com/rails/rails/issues/10051
+      redirect_to(sdk_path)
+    else
+      redirect_to("#{asset_host}#{sdk_path}")
+    end
   end
 end
