7c7845fa9b3b39a590ba846a396761897d3a50ff
Justin Searls <searls@gmail.com>
2014-07-01 18:37:30 -0400

    Serve up a mangled HTML response for facebot
    Facebook’s scraper does two things that defeats our shortlink mechanism:
    
    1. It follows redirects and discards shorturls along the way, meaning
    a user would be sharing a raw publisher URL and not the impaq.me 
    shortlink
    2. When a link-rel-canonical is set or when a meta-og:url is present in
    the HTML, facebook will scrape that URL instead of the shortlink. 
    
    This commit defeats #1 by fetching the HTML ourselves and proxying the
    response to users. It defeats #2 by mangling the HTML response with 
    regex. Yes, really.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :follow]
   before_action :check_user, except: :follow
+  layout 'links'
 
   def index
     @links = Link.all
@@ -12,7 +13,21 @@ class LinksController < ApplicationController
   end
 
   def follow
-    redirect_to Shortlink.follow(shortlink_params).to_s
+    @url = Shortlink.follow(shortlink_params).to_s
+    if params[:facebot] || request.user_agent.start_with?("facebookexternalhit")
+      url = URI.parse(@url)
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      @proxy_content = res.body.
+        gsub(/rel\=.?canonical.?/, 'rel="not-canonical"').
+        gsub(/property=.?og:url.?/, 'property="not:og:url"')
+
+      render :follow
+    else
+      redirect_to(@url)
+    end
   end
 
   private
