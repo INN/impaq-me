98aa83095a87f7fd218dd6d8e161b7fe07603458
Justin Searls <searls@gmail.com>
2014-07-30 11:00:51 -0400

    Put Rack::Cors in front of ActionDispatch::Static

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module ImpaqBackend
       "iframe" => "iframe"
     }
 
-    config.middleware.use Rack::Cors do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
