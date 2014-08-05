97e0ba3ce10fac34398204d264671c96f78c5590
Justin Searls <searls@gmail.com>
2014-08-05 11:46:49 -0400

    Creates a User & Campaign seed.
    The user login is simple, the first campaign is default. The second
    campaign is default but with two variants splitting traffic 70/30

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140804124644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "campaigns", force: true do |t|
     t.string   "mongo_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,71 @@
-if User.count == 0
-  user = User.new({ 
-    name: "Bobby Fancy",
-    email: "blah@blah.com",
-    password: "lookatme",
-    password_confirmation:"lookatme"
+def seed_users!
+  return if User.count > 0
+  User.create!({
+    name: "John Doe",
+    email: "user@example.com",
+    password: "password",
+    password_confirmation:"password"
   })
-  user.save!
+  puts <<-TEXT
+    Created User
+    =============
+    Email:    user@example.com
+    Password: password
+  TEXT
+end
+
+def seed_campaigns!
+  return if Campaign.count > 0
+
+  Campaign.create!(
+    :foundation_name => "Foundation A",
+    :publisher_name => "Publisher A",
+    :domains => ["localhost", "demo.testdouble.com"],
+    :goal => 2500.0,
+    :paypal_email => "user@example.com",
+    :twitter_username => "testdouble",
+    :variants => [
+      Variant.new(
+        :recommended_donation => 1.0,
+        :css_overrides => "",
+        :shown_amount => 100.0
+      )
+    ]
+  ).tap { |campaign| puts_model(campaign) }
+
+  Campaign.create!(
+    :foundation_name => "Foundation B (2 Variants)",
+    :publisher_name => "Publisher B",
+    :domains => ["127.0.0.1"],
+    :value_per_share => 2.0,
+    :value_per_click => 1.0,
+    :goal => 2500.0,
+    :paypal_email => "user@example.com",
+    :twitter_username => "inn",
+    :variants => [
+      Variant.new(
+        :recommended_donation => 1.0,
+        :css_overrides => "",
+        :shown_amount => 70.0
+      ),
+      Variant.new(
+        :recommended_donation => 1.0,
+        :css_overrides => "",
+        :shown_amount => 30.0
+      )
+    ]
+  ).tap { |campaign| puts_model(campaign) }
+end
+
+def puts_model(model)
+  puts <<-TEXT
+    Created #{model.class.name}
+    ===========================
+    #{model.attributes.map { |key, value| "      #{key.humanize}: #{value}" }.join("\n")}
+  TEXT
+end
+
+unless Rails.env.production?
+  seed_users!
+  seed_campaigns!
 end
