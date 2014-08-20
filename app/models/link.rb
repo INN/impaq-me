0ac38e68f131b770572e2543edcce387123178c6
Justin Searls <searls@gmail.com>
2014-08-20 15:20:27 -0400

    Query Articles less aggressively.
    If the link already has an article_id we don't need to query & load it.

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,22 @@ class Link < ActiveRecord::Base
 
   before_create :generate_slug, unless: "slug"
 
+  def self.find_or_create_for!(url, campaign, channel)
+    clean_url = CleansUrls.new(url).to_s
+
+    find_or_create_by!({
+      :campaign => campaign,
+      :long_url => clean_url,
+      :channel => channel
+    }).tap do |link|
+      unless link.article_id?
+        link.update!(
+          :article => Article.find_or_create_by!(:campaign => campaign, :url => clean_url)
+        )
+      end
+    end
+  end
+
   def generate_slug
     new_slug = SecureRandom.urlsafe_base64(7)
     slug_present?(new_slug) ? self.slug = new_slug : generate_slug
@@ -12,4 +28,5 @@ class Link < ActiveRecord::Base
   def slug_present?(new_slug)
     self.class.where(slug: new_slug).empty?
   end
+
 end
--- app/services/shortlink.rb
+++ app/services/shortlink.rb
@@ -3,14 +3,8 @@ require 'cleans_urls'
 
 class Shortlink
   def self.for_campaign_and_url(campaign, long_url)
-    long_url = CleansUrls.new(long_url).to_s
     Hash[['twitter', 'facebook', 'email'].map do |channel|
-      ["#{channel}_shortlink".to_sym, "l/#{Link.find_or_create_by({
-        :campaign => campaign,
-        :article => Article.find_or_create_by(:campaign => campaign, :url => long_url),
-        :long_url => long_url,
-        :channel => channel,
-      }).slug}"]
+      ["#{channel}_shortlink".to_sym, "l/#{Link.find_or_create_for!(long_url, campaign, channel).slug}"]
     end]
   end
 
